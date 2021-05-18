6ff0cfb3809c3a4141ea4785305e69faee6f1938
rolled-potatoes <keep4404@gmail.com>
2021-05-18 15:28:22 +0900

    feat : room 브로드 캐스트 로직 생성

--- src/main/kotlin/com/devcrown/talkietalkie/controller/MessageController.kt
+++ src/main/kotlin/com/devcrown/talkietalkie/controller/MessageController.kt
@@ -5,7 +5,9 @@ import java.security.Principal
 import java.util.*
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
 class MessageController(private var hashTagProcessor: HashTagProcessor) {
@@ -17,5 +19,11 @@ class MessageController(private var hashTagProcessor: HashTagProcessor) {
         if (hashTag != null) {
             hashTagProcessor.pushData(hashTag, priincipal.getName())
         }
+    }
+
+    @MessageMapping("/room/{token}")
+    @SendTo("/topic/room/{token}")
+    fun temp(@PathVariable token: String, @Payload message: ClientMessage): Any {
+        return message
     }
 }
